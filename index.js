af9af0da59e9000e41982f803537e6c244de87b6
Matthew Oluwajuwon <matthew.oluwajuwon@xpresspayments.com>
2024-02-19 15:18:53 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Here you would perform the actual authentication logic, like sending a request to a server.
+    // For the sake of simplicity, we're just logging the credentials to the console.
+    console.log('Username:', username);
+    console.log('Password:', password);
+});
